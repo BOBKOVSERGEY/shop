87af8cfcb709af9718447d8dfb297d25f3738a73
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-03-21 08:27:36 +0300

    Подключил шаблон главной и шаблон карточки товара

--- config/routes.php
+++ config/routes.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
+    'product/([0-9]+)' => 'product/view/$1', // actionView в ProductController
     '' => 'site/index' // actionIndex в SiteController
 ];
---
+++ controllers/ProductController.php
@@ -0,0 +1,11 @@
+<?php
+
+class ProductController
+{
+    public function actionView($id)
+    {
+        require_once ROOT . '/views/product/view.php';
+
+        return true;
+    }
+}
